6d18375a54c6a2af0437081877778c9688fb1cad
JuanLiguK <bl0016@est.ort.edu.ar>
2025-09-28 12:56:43 -0300

    COMIT ANDA LO PRIMERO

--- Assets/Scripts/AgentScript.cs
+++ Assets/Scripts/AgentScript.cs
@@ -6,48 +6,85 @@ using UnityEngine.AI;
 public class AgentScript : MonoBehaviour
 {
     NavMeshAgent agent;
-    [SerializeField] Transform[] patrolPoints;
-    [SerializeField] bool isPatroling = true;
-    [SerializeField] float arrivalDistance = 1;
-    [SerializeField] Animator anim;
-    [SerializeField] float velocity;
-    [SerializeField] Transform currentDestination;
-    [SerializeField] int currntPatrolPointIndex;
-    [SerializeField] Transform Player;
-    [SerializeField] RaycastSight raycastsight;
 
-    private void Awake()
+    [Header("Patrol")]
+    [SerializeField] Transform[] patrolPoints; // Points to patrol
+    [SerializeField] float arrivalDistance = 1f;
+
+    [Header("Chase")]
+    [SerializeField] Transform player; // Player transform
+    [SerializeField] Transform rayOrigin; // Object from which the raycast is fired
+    [SerializeField] float rayLength = 10f;
+    [SerializeField] float lostTime = 2f; // Time to lose sight before returning to patrol
+
+    Animator animator = null; // Animator of the NPC
+
+    int currentPatrolIndex = 0;
+    Transform currentDestination;
+
+    bool isChasing = false;
+    float lastSeenTime;
+
+    void Awake()
     {
         agent = GetComponent<NavMeshAgent>();
     }
-    // Start is called before the first frame update
+
     void Start()
     {
-        currentDestination = patrolPoints[0];
-        currntPatrolPointIndex = 0;
+        if (patrolPoints.Length > 0)
+        {
+            currentPatrolIndex = 0;
+            currentDestination = patrolPoints[currentPatrolIndex];
+            agent.destination = currentDestination.position;
+        }
     }
 
-    // Update is called once per frame
     void Update()
     {
-        if (agent.hasPath && agent.remainingDistance <= arrivalDistance)
+        DetectPlayer();
+
+        if (isChasing)
         {
-            if (currntPatrolPointIndex < patrolPoints.Length -1)
-            {
-                currntPatrolPointIndex++;
-            }
-            else
-            {
-                currntPatrolPointIndex = 0;
-            }
-            if (raycastsight.raycastHit == "Player")
-            {
-                currentDestination = Player;
-            }
-            currentDestination = patrolPoints[currntPatrolPointIndex];
+            agent.destination = player.position;
         }
-        agent.destination = currentDestination.position;
-        velocity = agent.velocity.magnitude;
-        anim.SetFloat("Speed",velocity);
+        else
+        {
+            // Normal patrol
+            if (!agent.pathPending && agent.remainingDistance <= arrivalDistance)
+            {
+                currentPatrolIndex++;
+                if (currentPatrolIndex >= patrolPoints.Length)
+                {
+                    currentPatrolIndex = 0;
+                }
+
+                currentDestination = patrolPoints[currentPatrolIndex];
+                agent.destination = currentDestination.position;
+            }
+        }
+
+
+        Debug.DrawRay(rayOrigin.position, rayOrigin.forward * rayLength, Color.red);
+    }
+
+    void DetectPlayer()
+    {
+        RaycastHit hit;
+        if (Physics.Raycast(rayOrigin.position, rayOrigin.forward, out hit, rayLength))
+        {
+            if (hit.transform == player)
+            {
+                isChasing = true;
+                lastSeenTime = Time.time;
+            }
+        }
+        else
+        {
+            if (Time.time - lastSeenTime > lostTime)
+            {
+                isChasing = false;
+            }
+        }
     }
 }
---
+++ Assets/Scripts/RaycastSight.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RayCastSight : MonoBehaviour
+{
+
+    [SerializeField] Transform originTR;
+    [SerializeField] float rayLength;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        originTR = GetComponent<Transform>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //lo primero que necesitamos es crear una variable de raycasthit que por defecto su nombre es hit --> almacena los valores que me devuelve el raycast cuando apuntoa a un collider
+
+        RaycastHit hitInfo;
+        if (Physics.Raycast(originTR.position, originTR.forward, out hitInfo, rayLength))
+        {
+            if (hitInfo.collider.CompareTag("Player"))
+            {
+                Debug.Log("Jugador detectado");
+            }
+        }
+    }
+
+    void OnDrawGizmos() // ESPECIE DE UPDATE que esta todo el tiempo ejecutandose, gizmos son ayudas visuales que tiene unity
+    {
+        Color color = Color.red;
+        Gizmos.color = color;
+        Gizmos.DrawLine(originTR.position, originTR.position + originTR.forward * rayLength);
+    }
+
+}
